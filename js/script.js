a5c55f3bd95df354ace199f36cc2f5bb55cdc6a1
Harsh-09A <harsh.autowebbed@gmail.com>
2025-08-20 13:58:48 +0530

    Starter File

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Toggle Content Button
+function toggleContent(dotsId, moreTextId, btnId) {
+  var dots = document.getElementById(dotsId);
+  var moreText = document.getElementById(moreTextId);
+  var btnText = document.getElementById(btnId);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More ...";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+}
+
+// Dynamic modal title based on which button was clicked
+const leadModal = document.getElementById("popupModal");
+if (leadModal) {
+  leadModal.addEventListener("show.bs.modal", function (event) {
+    const button = event.relatedTarget;
+    const modalTitle = button.getAttribute("data-modal-title");
+    const modalTitleElement = leadModal.querySelector(".modal-title");
+
+    if (modalTitle && modalTitleElement) {
+      modalTitleElement.textContent = modalTitle;
+    }
+  });
+}
